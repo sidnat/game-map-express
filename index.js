a538896d57454db7302804efb7a1563d5681acd5
Sid Natarajan <sid.nat@gmail.com>
2023-01-25 23:33:58 -0500

    implemented getMap, addPin, getPin routes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser').json()
 const { v4: uuidv4 } = require('uuid');
 const db = require('./services/db');
 var cors = require('cors')
-const { addMap, getMap } = require('./game-map-helpers')
+const { addMap, getMap, addPin, getPinsWithMapID } = require('./game-map-helpers')
 app.use(cors())
 
 var corsOptions = {
@@ -13,7 +13,7 @@ var corsOptions = {
   // optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.post('/save', [bodyParser, cors(corsOptions)], (req, res) => {
+app.post('/addMap', [bodyParser, cors(corsOptions)], (req, res) => {
   console.log(req.body)
   const uuid = uuidv4();
   const mapName = req.body.mapName;
@@ -27,15 +27,59 @@ app.post('/save', [bodyParser, cors(corsOptions)], (req, res) => {
   res.send()
 })
 
-app.get("/getMapData", (req, res) => {
-  const { mapName, creator, imageLink } = getMap(req.params.uuid)
-  console.log(getMap(req.params.uuid))
+app.post('/addPin', [bodyParser, cors(corsOptions)], (req, res) => {
+  console.log(req.body)
+  const uuid = uuidv4();
+  const mapID = req.body.mapID;
+  const xCoordinate = req.body.xCoordinate;
+  const yCoordinate = req.body.yCoordinate;
+  const colour = req.body.colour;
+  const timestamp = req.body.timestamp; //YYYY-MM-DD HH:MI:SS
+  const title = req.body.title;
+  const note = req.body.note;
+  const screenshot = req.body.screenshot;
 
-  // fix route so i properly get the sql data for specific uuid
+  addPin(uuid, mapID, xCoordinate, yCoordinate, colour, timestamp, title, note, screenshot)
+  res.send()
+})
 
-  // need to use res.send(body)  
+app.get("/getMap", async (req, res) => {
+  // const { mapName, creator, imageLink } = getMap(req.query.uuid)
+  // console.log(req.query)
+  // console.log(getMap(req.params.uuid))
+  const map = await getMap(req.query.uuid)
+  console.log('map', map)
+  res.send(map)
 
-}) //retrieve particular database entry by UUID
+  // {
+  //   id: '184b9f7a-91c2-48ab-953c-b0e3182f7280',
+  //   map_name: 'rdr2',
+  //   creator: 'dindang',
+  //   image_link: 'www.google.com'
+  // }
+})
+
+app.get("/getPin", async (req, res) => {
+  // const { mapName, creator, imageLink } = getMap(req.query.uuid)
+  // console.log(req.query)
+  // console.log(getMap(req.params.uuid))
+  const pins = await getPinsWithMapID(req.query.mapID)
+  res.send(pins)
+
+  // [
+  //   {
+  //     id: 0,
+  //     map_id: 184,
+  //     x_coordinate: 10,
+  //     y_coordinate: 10,
+  //     colour: '29f0ad',
+  //     title: 'come back for treasure',
+  //     note: '2023-01-24 16:42:32',
+  //     timestamp: Invalid Date,
+  //     screenshot: 'https://static01.nyt.com/images/2018/11/25/opinion/25SUDERMAN/25SUDERMAN-superJumbo.jpg?quality=75&auto=webp'
+  //   }
+  // ]
+})
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
